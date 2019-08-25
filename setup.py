d9a50a1f229958fe24205c874af035a48cd17db0
abdurrehman.nadeem <abdurrehman245@gmail.com>
2019-08-25 17:01:42 +0500

    new release of package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kaggle_utility',
-    version='1.0',
+    version='1.1',
     author='Abdur Rehman Nadeem',
     description='A utility for kaggle machine learning projects',
     license='MIT',
